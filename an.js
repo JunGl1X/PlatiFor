abbcd65beadfc747faa1682c723caad07c46f63b
JunGl1X <bogdanthekopcha@mail.ru>
2017-05-08 17:04:05 +0300

    Add files via upload

---
+++ an.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+
+  $('#invite_can').on('click', function () {
+
+    $('#invite_thing').animate({height:"toggle"},2500)
+    ;
+})
+
+
+
+
+$('#free_shop_code').mouseover({a:12, b:"abc"}, function(eventObject){
+
+  $('.over').hide("slow");  /*hide*/
+  $('#over_code').hide("slow");
+
+  $('.out').show("slow");  /*show*/
+  $('#out_code').show("slow");
+
+});
+
+$('#free_shop_code').mouseleave({a:12, b:"abc"}, function(eventObject){
+
+  $('.out').hide("slow");   /*hide*/
+  $('#out_code').hide("slow");
+
+  $('.over').show("slow");  /*show*/
+  $('#over_code').show("slow");
+
+})
+
+})
---
+++ animation_shop.js
@@ -0,0 +1,171 @@
+$(document).ready(function () {
+
+
+
+  $(".container_banner_with_imgres").mouseover( function () {
+    $(".banners_With_imgres_st_banner_one").css({"background-color":"lightblue"});
+  });
+  $(".container_banner_with_imgres").mouseout( function () {
+    $(".banners_With_imgres_st_banner_one").css({"background-color":"black"});
+  });
+
+
+
+
+  $(".container_banner_with_imgres2").mouseover( function () {
+    $(".banners_With_imgres_st_banner_second").css({"background-color":"lightblue"});
+  });
+  $(".container_banner_with_imgres2").mouseout( function () {
+    $(".banners_With_imgres_st_banner_second").css({"background-color":"black"});
+  });
+
+
+
+
+  $(".container_banner_with_imgres3").mouseover( function () {
+    $(".banners_With_imgres_st_banner_third").css({"background-color":"lightblue"});
+  });
+  $(".container_banner_with_imgres3").mouseout( function () {
+    $(".banners_With_imgres_st_banner_third").css({"background-color":"black"});
+  });
+
+
+
+
+
+
+
+
+
+$(".bestsellers_bags_and_accessories").on("click" , function () {
+
+$(".bestsellers_bags_and_accessories").css({"color":"#ADD8E6"});
+  $(".primary_image_first_row_first").attr("src","https://livedemo00.template-help.com/opencart_61111_v2/image/cache/catalog/bags/product-31-270x280.png");
+  $(".primary_image_first_row_second").attr("src","https://livedemo00.template-help.com/opencart_61111_v2/image/cache/catalog/bags/product-37-270x280.png");
+  $(".primary_image_first_row_third").attr("src","https://livedemo00.template-help.com/opencart_61111_v2/image/cache/catalog/bags/product-7-270x280.png");
+  $(".first_problemss").css({"display":"none"});
+  $(".second_problemsss").css({"display":"none"});
+  $(".first_bag_fst").css({"display":"none"});
+  $(".display_none_third_etc").css({"display":"none"});
+
+  $(".second_row_bags_and_accessories").css({"display":"block"});
+  $(".background_bags3").css({"display":"block"});
+  $(".background_bags2").css({"display":"block"});
+
+  $(".primary_image_first_row_second").css({"display":"block"});
+  $(".primary_image_first_row_third").css({"display":"block"});
+  $(".dn_last_bags").css({"display":"block"});
+$(".second_hernya").css({"position":"relative" , "left":"60px" , "top":"5px"});
+$(".new_bags_and_accessories").css({"color":"#000"});
+$(".specials_bags_and_accessories").css({"color":"#000"});
+$(".featured_bags_and_accessories").css({"color":"#000"});
+
+$(".bestsellers_bags_and_accessories").css({"border-width":"2px"});
+$(".specials_bags_and_accessories").css({"border-width":"0px"});
+$(".new_bags_and_accessories").css({"border-width":"0px"});
+$(".featured_bags_and_accessories").css({"border-width":"0px"});
+$(".news_banners_little").css({"display":"none"});
+$(".sale_banners_little").css({"display":"none"});
+
+
+});
+
+
+$(".featured_bags_and_accessories").on("click" , function () {
+  $(".featured_bags_and_accessories").css({"color":"#ADD8E6"});
+
+  $(".primary_image_first_row_first").attr("src","https://livedemo00.template-help.com/opencart_61111_v2/image/cache/catalog/bags/product-25-270x280.png");
+  $(".primary_image_first_row_second").attr("src","https://livedemo00.template-help.com/opencart_61111_v2/image/cache/catalog/bags/product-7-270x280.png");
+  $(".primary_image_first_row_third").attr("src","https://livedemo00.template-help.com/opencart_61111_v2/image/cache/catalog/bags/product-1-270x280.png");
+  $(".first_problemss").css({"display":"none"});
+  $(".second_problemsss").css({"display":"none"});
+  $(".first_bag_fst").css({"display":"none"});
+  $(".display_none_third_etc").css({"display":"none"});
+  $(".second_row_bags_and_accessories").css({"display":"block"});
+  $(".background_bags3").css({"display":"block"});
+  $(".background_bags2").css({"display":"block"});
+  $(".primary_image_first_row_second").css({"display":"block"});
+  $(".dn_last_bags").css({"display":"block"});
+  $(".primary_image_first_row_third").css({"display":"block"});
+
+  $(".new_bags_and_accessories").css({"color":"#000"});
+  $(".specials_bags_and_accessories").css({"color":"#000"});
+  $(".bestsellers_bags_and_accessories").css({"color":"#000"});
+
+  $(".bestsellers_bags_and_accessories").css({"border-width":"0px"});
+  $(".specials_bags_and_accessories").css({"border-width":"0px"});
+  $(".new_bags_and_accessories").css({"border-width":"0px"});
+  $(".featured_bags_and_accessories").css({"border-width":"2px"});
+  $(".news_banners_little").css({"display":"none"});
+  $(".sale_banners_little").css({"display":"none"});
+
+
+});
+
+
+$(".specials_bags_and_accessories").on("click" , function () {
+  $(".specials_bags_and_accessories").css({"color":"#ADD8E6"});
+
+  $(".primary_image_first_row_first").attr("src","https://livedemo00.template-help.com/opencart_61111_v2/image/cache/catalog/bags/product-4-270x280.png");
+  $(".primary_image_first_row_second").css({"display":"none"});
+  $(".primary_image_first_row_third").css({"display":"none"});
+  $(".first_problemss").css({"display":"none"});
+  $(".second_problemsss").css({"display":"none"});
+  $(".first_bag_fst").css({"display":"none"});
+  $(".dn_last_bags").css({"display":"none"});
+ $(".second_row_bags_and_accessories").css({"display":"none"});
+ $(".background_bags3").css({"display":"none"});
+ $(".background_bags2").css({"display":"none"});
+ $(".new_bags_and_accessories").css({"color":"#000"});
+ $(".featured_bags_and_accessories").css({"color":"#000"});
+ $(".bestsellers_bags_and_accessories").css({"color":"#000"});
+
+ $(".bestsellers_bags_and_accessories").css({"border-width":"0px"});
+ $(".specials_bags_and_accessories").css({"border-width":"2px"});
+ $(".new_bags_and_accessories").css({"border-width":"0px"});
+ $(".featured_bags_and_accessories").css({"border-width":"0px"});
+ $(".news_banners_little").css({"display":"none"});
+$(".sale_banners_little").css({"display":"block"});
+});
+
+
+$(".new_bags_and_accessories").on("click" , function () {
+  $(".new_bags_and_accessories").css({"color":"#ADD8E6"});
+  $(".specials_bags_and_accessories").css({"color":"#000"});
+  $(".featured_bags_and_accessories").css({"color":"#000"});
+  $(".bestsellers_bags_and_accessories").css({"color":"#000"});
+
+  $(".primary_image_first_row_first").attr("src","http://static.livedemo00.template-help.com/opencart_61111_v2/image/cache/catalog/bags/product-58-270x280.png");
+  $(".primary_image_first_row_second").attr("src","https://livedemo00.template-help.com/opencart_61111_v2/image/cache/catalog/bags/product-55-270x280.png");
+  $(".primary_image_first_row_third").attr("src","https://livedemo00.template-help.com/opencart_61111_v2/image/cache/catalog/bags/product-52-270x280.png");
+  $(".first_problemss").css({"display":"none"});
+  $(".second_problemsss").css({"display":"none"});
+  $(".first_bag_fst").css({"display":"none"});
+
+  $(".second_row_bags_and_accessories").css({"display":"block"});
+  $(".background_bags3").css({"display":"block"});
+  $(".background_bags2").css({"display":"block"});
+  $(".first_problemss").css({"display":"block"});
+  $(".second_problemsss").css({"display":"block"});
+  $(".first_bag_fst").css({"display":"block"});
+  $(".dn_last_bags").css({"display":"block"});
+
+  $(".primary_image_first_row_second").css({"display":"block"});
+  $(".primary_image_first_row_third").css({"display":"block"});
+
+  $(".bestsellers_bags_and_accessories").css({"border-width":"0px"});
+  $(".specials_bags_and_accessories").css({"border-width":"0px"});
+  $(".new_bags_and_accessories").css({"border-width":"2px"});
+  $(".featured_bags_and_accessories").css({"border-width":"0px"});
+  $(".news_banners_little").css({"display":"block"});
+  $(".sale_banners_little").css({"display":"none"});
+
+
+
+});
+
+
+
+
+
+});
